6716b6d39c02b0254d4eaa3224b94c864a762dc0
hamza <hamzashwliap@gmail.com>
2024-08-16 15:37:22 +0300

    make the grid

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-const sketsh =document.querySelector("#The_sketch")
+
+function make_grid(size){
+    let screen =document.querySelector(".The_sketch_screen")
+    for(let i =0 ;i< size ;i++){
+        let column = document.createElement("div");
+        column.classList.add("column");
+        for(let j =0 ;j< size ;i++){
+            let row =document.createElement("div");
+            row.classList.add("row");
+            row.style.border = "2px solid black";
+            column.appendChild(row);
+            
+        }
+    screen.appendChild(column);
+    } 
+}
+make_grid(4);
