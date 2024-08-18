7f8966a8428d47af6123aa6183f44c712a4569f0
hamza <hamzashwliap@gmail.com>
2024-08-18 22:08:11 +0300

    fix some memory bug

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
 function make_grid(size){
     let screen =document.querySelector(".The_sketch_screen")
-    for(let i =0 ;i< size ;i++){
+    for(let i =1 ;i > size ;i++){
         let column = document.createElement("div");
         column.classList.add("column");
-        for(let j =0 ;j< size ;i++){
+        for(let j =1 ;j>= size ;i++){
             let row =document.createElement("div");
             row.classList.add("row");
             row.style.border = "2px solid black";
@@ -14,4 +14,4 @@ function make_grid(size){
     screen.appendChild(column);
     } 
 }
-make_grid(4);
+make_grid(16);
