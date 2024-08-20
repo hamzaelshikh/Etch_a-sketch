e01ac7094254c3b0764228a5d32dc5d5e8593468
hamza <hamzashwliap@gmail.com>
2024-08-20 22:34:42 +0300

    fix a bug

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
 function make_grid(size){
     let screen =document.querySelector(".The_sketch_screen")
-    for(let i =1 ;i > size ;i++){
+    for(let i =1 ;i <= size ;i++){
         let column = document.createElement("div");
         column.classList.add("column");
-        for(let j =1 ;j>= size ;i++){
+        for(let j =1 ;j<= size ;j++){
             let row =document.createElement("div");
             row.classList.add("row");
             row.style.border = "2px solid black";
